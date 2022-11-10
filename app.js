3fb649d12c1bca1e38c55672db1db7de7122282f
Valeriia.Kurylko <reviliya@gmail.com>
2022-11-11 00:10:09 +0100

    delete added

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", userHandlers.postUser);
+//app.post("/api/movies", movieHandlers.postMovie);
+//app.post("/api/users", userHandlers.postUser);
 
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 app.post("/api/users", validateUser, userHandlers.postUser);
@@ -32,7 +32,8 @@ app.post("/api/users", validateUser, userHandlers.postUser);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", userHandlers.updateUser);
 
-
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
